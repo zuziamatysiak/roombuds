7190bc1ef32da7ea0e9f0c867c36da8bb2726d11
audrey-yang <ayang24.robin@gmail.com>
2023-04-02 12:02:58 -0400

    Add matches code

--- roombuds/components/Navbar.tsx
+++ roombuds/components/Navbar.tsx
@@ -90,7 +90,7 @@ export const Navbar = () => {
               }}
               variant="contained"
               onClick={() => {
-                router.push('/explore')
+                router.push('/matches')
               }}
             >
               Matches
